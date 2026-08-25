bb73bda6f2eb7a7cf88b6f85531895e426de7edb
Anupriya <anupriya6115@gmail.com>
2026-08-25 22:19:53 +0530

    Add Chemistry [B] from Codeforces

---
+++ 1883/B - Chemistry/solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            String s = sc.next();
+ 
+            int[] freq = new int[26];
+ 
+            for (char ch : s.toCharArray()) {
+                freq[ch - 'a']++;
+            }
+ 
+            int odd = 0;
+ 
+            for (int count : freq) {
+                if (count % 2 != 0) {
+                    odd++;
+                }
+            }
+ 
+            if (odd <= k + 1) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+ 
+        sc.close();
+    }
+}
