d304b48c1d3fbece148822ee6733c2c5ec896d08
Anupriya <anupriya6115@gmail.com>
2026-09-04 19:46:31 +0530

    Add Make It Increasing [B] from Codeforces

---
+++ 1675/B - Make It Increasing/solution.java
@@ -0,0 +1,85 @@
+import java.io.*;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner(System.in);
+        StringBuilder out = new StringBuilder();
+ 
+        int t = fs.nextInt();
+ 
+        while (t-- > 0) {
+            int n = fs.nextInt();
+            long[] a = new long[n];
+ 
+            for (int i = 0; i < n; i++) {
+                a[i] = fs.nextLong();
+            }
+ 
+            long operations = 0;
+            boolean possible = true;
+ 
+            for (int i = n - 2; i >= 0; i--) {
+                while (a[i] >= a[i + 1] && a[i] > 0) {
+                    a[i] /= 2;
+                    operations++;
+                }
+ 
+                if (a[i] >= a[i + 1]) {
+                    possible = false;
+                    break;
+                }
+            }
+ 
+            if (possible) {
+                out.append(operations).append('
+');
+            } else {
+                out.append(-1).append('
+');
+            }
+        }
+ 
+        System.out.print(out);
+    }
+ 
+    static class FastScanner {
+        private final InputStream in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+ 
+        FastScanner(InputStream in) {
+            this.in = in;
+        }
+ 
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len == -1) return -1;
+            }
+            return buffer[ptr++];
+        }
+ 
+        long nextLong() throws IOException {
+            int c;
+ 
+            do {
+                c = read();
+            } while (c <= ' ');
+ 
+            long res = 0;
+ 
+            while (c > ' ') {
+                res = res * 10 + (c - '0');
+                c = read();
+            }
+ 
+            return res;
+        }
+ 
+        int nextInt() throws IOException {
+            return (int) nextLong();
+        }
+    }
+}
