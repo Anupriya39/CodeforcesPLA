f924a6ecf53f04d9ff4d9df8c5fc49e6dd8517ab
Anupriya <anupriya6115@gmail.com>
2026-09-14 21:51:25 +0530

    Add Multiply by 2, divide by 6 [B] from Codeforces

---
+++ 1374/B - Multiply by 2, divide by 6/solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            long n = sc.nextLong();
+ 
+            int count2 = 0;
+            int count3 = 0;
+ 
+            while (n % 2 == 0) {
+                n /= 2;
+                count2++;
+            }
+ 
+            while (n % 3 == 0) {
+                n /= 3;
+                count3++;
+            }
+ 
+            // Other prime factors exist
+            if (n != 1 || count2 > count3) {
+                System.out.println(-1);
+            } else {
+                System.out.println(2 * count3 - count2);
+            }
+        }
+ 
+        sc.close();
+    }
+}
